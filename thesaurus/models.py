cb53d5ec969371ecfaa73eb4ac2ae9be77f3a9bf
falbrito <britofa@paho.org>
2017-11-14 09:00:05 -0200

    Concept proof of DeCS system, first commit

--- thesaurus/models.py
+++ thesaurus/models.py
@@ -4,19 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# thesaurus fields
-class Thesaurus(models.Model):
-
-	class Meta:
-			verbose_name = u'Thesaurus'
-			verbose_name_plural = u'Thesaurus'
-
-	thesaurus_name = models.CharField(u'Thesaurus name', max_length=250, blank=False)
-
-	thesaurus_author = models.CharField(u'Author', max_length=250, blank=False)
-
-	thesaurus_scope = models.CharField(u'Scope', max_length=250, blank=False)
+from .models_thesaurus import *
+from .models_qualifiers import *
+from .models_descriptors import *
 
 
-	def __str__(self):
-		return '%s' % (self.thesaurus_name)
